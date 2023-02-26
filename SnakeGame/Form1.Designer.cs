36b8d3f9021361625e51dd1f9647a5caad826264
your_username <nguyenthanhloi260303@gmail.com>
2023-02-26 22:38:20 +0700

    fist commit

---
+++ SnakeGame/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SnakeGame
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 425);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(796, 403);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SnakeGame/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace SnakeGame
+{
+    public partial class MainForm : Form
+    {
+        private const int SnakeWidth = 10;
+        private const int SnakeHeight = 10;
+        private const int FoodWidth = 10;
+        private const int FoodHeight = 10;
+
+        private Snake snake;
+        private Food food;
+        private Random random;
+        private int score=0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            random = new Random();
+            NewGame();
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                snake.Direction = Direction.Left;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                snake.Direction = Direction.Right;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                snake.Direction = Direction.Up;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                snake.Direction = Direction.Down;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            snake.Move();
+
+            if (snake.CollidesWith(food))
+            {
+                snake.Grow();
+                score += 10;
+                label1.Text = "Score: " + score;
+                PlaceFood();
+            }
+            else if (snake.CollidesWithSelf())
+            {
+                timer1.Stop();
+                MessageBox.Show("Game Over");
+                NewGame();
+            }
+            else if (snake.CollidesWithWall(pictureBox1.ClientSize))
+            {
+                timer1.Stop();
+                MessageBox.Show("Game Over");
+                NewGame();
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            snake.Draw(e.Graphics);
+            food.Draw(e.Graphics);
+        }
+
+        private void NewGame()
+        {
+            snake = new Snake();
+            snake.InitialPosition();
+            PlaceFood();
+            timer1.Start();
+        }
+
+        private void PlaceFood()
+        {
+            int x = random.Next(pictureBox1.ClientSize.Width / SnakeWidth) * SnakeWidth;
+            int y = random.Next(pictureBox1.ClientSize.Height / SnakeHeight) * SnakeHeight;
+            food = new Food(x, y, FoodWidth, FoodHeight);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Score: " + score;
+        }
+    }
+
+    public class Snake
+    {
+        private const int StepSize = 10;
+
+        private readonly List<Point> body;
+        private bool isGrowing;
+
+        public Snake()
+        {
+            body = new List<Point>();
+            Direction = Direction.Right;
+        }
+
+        public Direction Direction { get; set; }
+
+        public void InitialPosition()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Point point = new Point(10 - i, 0);
+                body.Insert(0, point);
+            }
+        }
+
+        public void Move()
+        {
+            Point head = body[0];
+            Point newHead = new Point(head.X, head.Y);
+
+            if (Direction == Direction.Left)
+            {
+                newHead.X -= StepSize;
+            }
+            else if (Direction == Direction.Right)
+            {
+                newHead.X += StepSize;
+            }
+            else if (Direction == Direction.Up)
+            {
+                newHead.Y -= StepSize;
+            }
+            else if (Direction == Direction.Down)
+            {
+                newHead.Y += StepSize;
+            }
+
+            body.Insert(0, newHead);
+            if (!isGrowing)
+            {
+                body.RemoveAt(body.Count - 1);
+            }
+            else
+            {
+                isGrowing = false;
+            }
+        }
+
+        public bool CollidesWith(Point point)
+        {
+            return body.Any(b => b == point);
+        }
+
+        public bool CollidesWithSelf()
+        {
+            Point head = body[0];
+            return body.Skip(1).Any(b => b == head);
+        }
+
+        public bool CollidesWithWall(Size bounds)
+        {
+            Point head = body[0];
+            return head.X < 0 || head.Y < 0 || head.X >= bounds.Width || head.Y >= bounds.Height;
+        }
+
+        public bool CollidesWith(Food food)
+        {
+            return CollidesWith(food.Location);
+        }
+
+        public void Grow()
+        {
+            isGrowing = true;
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            for (int i = 0; i < body.Count; i++)
+            {
+                Brush brush = i == 0 ? Brushes.Black : Brushes.Gray;
+                graphics.FillRectangle(brush, new Rectangle(body[i].X, body[i].Y, StepSize, StepSize));
+            }
+        }
+    }
+
+    public class Food
+    {
+        public Food(int x, int y, int width, int height)
+        {
+            Location = new Point(x, y);
+            Width = width;
+            Height = height;
+        }
+
+        public Point Location { get; }
+        public int Width { get; }
+        public int Height { get; }
+
+        public void Draw(Graphics graphics)
+        {
+            graphics.FillRectangle(Brushes.Red, new Rectangle(Location.X, Location.Y, Width, Height));
+        }
+    }
+
+    public enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+}
